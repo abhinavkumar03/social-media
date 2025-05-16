8680b6052a7c4112bc727460f461c9d6bba5979b
Abhinav <abhinav@Admins-MacBook-Air.local>
2025-05-16 10:47:12 +0530

    fix2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,16 @@ module.exports = {
       PM2_SERVE_PORT: 3000,
       PM2_SERVE_SPA: 'true',
       PM2_SERVE_HOMEPAGE: '/index.html'
-    }
+    },
+    watch: false,
+    max_memory_restart: '1G',
+    exp_backoff_restart_delay: 100,
+    error_file: 'logs/err.log',
+    out_file: 'logs/out.log',
+    merge_logs: true,
+    log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+    autorestart: true,
+    max_restarts: 10,
+    restart_delay: 4000
   }]
 } 
