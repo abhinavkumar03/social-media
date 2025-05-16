341c490fb6d1d00c6e389f68eea4414b690b3d5f
Abhinav <abhinav@Admins-MacBook-Air.local>
2025-05-16 11:05:24 +0530

    fix3

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://media-bend.onrender.com/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://13.203.223.187:5000/api';
 
 const api = axios.create({
   baseURL: API_URL,
